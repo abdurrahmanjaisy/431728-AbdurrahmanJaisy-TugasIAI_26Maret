ffa0ac76f45571ae3da0e531fa7ff25c237d1403
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-03-28 20:26:07 +0700

    ganti model

--- app/Git.php
+++ app/Git.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-
-class User extends Model
+class Git extends Model
 {
     protected $fillable = [
         'id', 'user_id', 'git', 'created_at', 'updated_at',
